14cbe94562290a273fab7cfe224eabe8c6f83c52
김민성 <msung98@naver.com>
2026-04-30 01:07:21 +0900

    fix : auditing 위치변경

---
+++ alert_system/src/main/java/src/alert_system/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package src.alert_system.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
