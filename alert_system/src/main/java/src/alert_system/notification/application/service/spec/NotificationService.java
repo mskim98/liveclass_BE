ea59695354311395a6db63ab363f080486fe5c1e
김민성 <msung98@naver.com>
2026-05-01 20:28:08 +0900

    feat : 알림 서비스 스펙 추가

--- alert_system/src/main/java/src/alert_system/notification/application/service/spec/NotificationService.java
+++ alert_system/src/main/java/src/alert_system/notification/application/service/spec/NotificationService.java
@@ -1,11 +1,22 @@
 package src.alert_system.notification.application.service.spec;
 
 import src.alert_system.notification.api.dto.NotificationResponse;
+import src.alert_system.notification.api.dto.UserNotificationResponse;
 import src.alert_system.notification.application.command.NotificationCreateCommand;
+import src.alert_system.notification.domain.enums.Channel;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface NotificationService {
 
     NotificationResponse createNotification(NotificationCreateCommand command);
 
     NotificationResponse findById(String notificationId);
+
+    List<UserNotificationResponse> findUserNotifications(UUID userId, Channel channel, Boolean readFilter);
+
+    void markRead(UUID userId, Long deliveryId);
+    
+    void retryDeadDelivery(String notificationId, Long deliveryId);
 }
